75e6c2a135dacd0ddd94e16ebfd1fda842ef2898
YardenGabbay <117689805+YardenGabbay@users.noreply.github.com>
2025-07-14 16:50:56 +0300

    Update main_naive_greedy.py

--- main_naive_greedy.py
+++ main_naive_greedy.py
@@ -8,7 +8,6 @@ if __name__=="__main__":
     treatment = ""
     outcome = ""
     confounders = []
-    attributes_for_random_walks = []
     desired_ate = None
     epsilon= None
     approx = False
