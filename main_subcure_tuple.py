f538e1a8bfa455f9521023dc8122194c113bb3f4
YardenGabbay <117689805+YardenGabbay@users.noreply.github.com>
2025-07-16 22:57:07 +0300

    Update main_subcure_tuple.py

--- main_subcure_tuple.py
+++ main_subcure_tuple.py
@@ -2,7 +2,7 @@ from clustered_top_k import ClusteredATEBaselines
 from batch_sampled_top_k import BatchSampledATEBaselines
 import pandas as pd
 
-if __name__=="__main__":
+def main():
     # params
     csv_name = ""
     treatment = ""
@@ -27,6 +27,6 @@ if __name__=="__main__":
     else:
         sampled_baselines = ClusteredATEBaselines(df[confounders], df[treatment], df[outcome], model_type=model_type)
         sampled_topk_results = sampled_baselines.top_k_plus(target_ate=desired_ate, epsilon=epsilon, approx=approx, influence_recalc_interval=influence_recalc_interval)
-
-        
-
+    
+if __name__=="__main__":
+    main()
