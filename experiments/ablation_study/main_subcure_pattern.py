80e987d3ac335b9339bfaf8a33bd7ce9ac8e35eb
YardenGabbay <117689805+YardenGabbay@users.noreply.github.com>
2025-07-17 00:21:24 +0300

    Create main_subcure_pattern.py

---
+++ experiments/ablation_study/main_subcure_pattern.py
@@ -0,0 +1,17 @@
+from subcure_pattern import main
+
+if __name__=="__main__":
+    csv_name = ""
+    treatment = ""
+    outcome = ""
+    confounders = []
+    desired_ate = None
+    epsilon= None
+
+    # default params - can be changed
+    k=1000
+    size_threshold=0.2
+    weights_optimization_method = 1 # 0- no optimization, 1- sorting, 2- real weights
+    df = pd.read_csv(csv_name)
+    attributes_for_random_walks = [c for c in df.columns if c not in [treatment, outcome]]
+    main(csv_name, attributes_for_random_walks, confounders, treatment, outcome, desired_ate, k, size_threshold, weights_optimization_method, epsilon)
